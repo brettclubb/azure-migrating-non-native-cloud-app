bfa1413ad4a8209d5c60f88684910910a81f3d1c
brettclubb <brett.clubb@gmail.com>
2021-05-01 14:15:20 -0500

    Adds screenshots

--- function/ServiceBusQueueTrigger1/__init__.py
+++ function/ServiceBusQueueTrigger1/__init__.py
@@ -12,25 +12,33 @@ def main(msg: func.ServiceBusMessage):
     logging.info('Python ServiceBus queue trigger processed message: %s',notification_id)
 
     # Get connection to database
-    conn = psycopg2.connect("dbname=techconfdb user=webapp")
+    host = "nnc-postgres.postgres.database.azure.com"
+    dbname = "techconfdb"
+    user = "webapp@nnc-postgres"
+    password = "V3ryStr0ngP@$$"
+    sslmode = "require"
+    conn_string = "host={0} user={1} dbname={2} password={3} sslmode={4}".format(host, user, dbname, password, sslmode)
+    conn = psycopg2.connect(conn_string)
 
     try:
         cur = conn.cursor()
 
         # Get notification message and subject from database using the notification_id
-        notification = cur.Execute("SELECT message, subject FROM notification WHERE id = %s;", notification_id)
+        cur.execute("SELECT * FROM notification WHERE id = %s;", notification_id)
+        notification = cur.fetchAll()
 
         # Get attendees email and name
-        attendees = cur.Execute("SELECT email, name FROM attendee;")
+        cur.execute("SELECT * FROM attendee;")
+        attendees = cur.fetchAll()
 
         # Loop through each attendee and send an email with a personalized subject
         for attendee in attendees:
-            subject = ("%s - %s", (attendee.name, notification.subject))
-            content = Content("text/plain", notification.message)
-            Mail("from@email.com", attendee.email, subject, content)
+            subject = subject = '{}: {}'.format(attendee.first_name, notification.subject)
+            #send_email(attendee.email, subject, notification.message)
 
         # Update the notification table by setting the completed date and updating the status with the total number of attendees notified
-        cur.Execute("UPDATE notification SET completed_date = %s, status = %s ;", (datetime.now, len(attendees)))
+        notified_count = 'Notified {} attendees'.format(len(attendees))
+        cur.Execute("UPDATE notification SET completed_date = %s, status = %s WHERE id = %s ;", (datetime.utcnow(), notified_count, notification_id))
 
     except (Exception, psycopg2.DatabaseError) as error:
         logging.error(error)
